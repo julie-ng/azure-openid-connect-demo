0b7ef14efe4385b4c51701ff63a4650d915ed6c2
Julie Ng <me@julie.io>
2019-09-28 17:52:19 +0200

    add example dotenv

--- app/server.js
+++ app/server.js
@@ -1,6 +1,11 @@
 'use strict'
 const app = require('./app')
 const http = require('http')
+
+if (process.env.NODE_ENV === 'development') {
+	require('dotenv').config()
+}
+
 const port = process.env.PORT || 3000
 
 const server = http.createServer(app)
