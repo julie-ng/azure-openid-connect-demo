b35736f0a50e29555b37b25dbb16f334be108ff9
Julie Ng <me@julie.io>
2019-10-09 15:52:35 +0200

    oidc: reduce claims to test using work accounts

--- app/config/oidc.js
+++ app/config/oidc.js
@@ -60,7 +60,7 @@ module.exports = {
   // 'profile' scope is required, the rest scopes are optional.
   // (1) if you want to receive refresh_token, use 'offline_access' scope
   // (2) if you want to get access_token for graph api, use the graph api url like 'https://graph.microsoft.com/mail.read'
-  scope: ['profile', 'offline_access', 'https://graph.microsoft.com/mail.read'],
+  scope: ['profile'],
 
   // Optional, 'error', 'warn' or 'info'
   loggingLevel: 'info',
