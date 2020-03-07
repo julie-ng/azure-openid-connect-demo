53f96cb4c90c3d22263e61bf0706802da80171b8
Julie Ng <me@julie.io>
2020-03-07 08:57:39 +0100

    fix(redis): explicitly connect with TLS

--- app/middleware/session.js
+++ app/middleware/session.js
@@ -9,42 +9,47 @@ const REDIS_PORT = process.env.REDIS_PORT || 6379
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD || false
 const SESSION_SECRET = process.env.SESSION_SECRET || 'keyboard cat'
 
+const clientOpts = {
+	host: REDIS_HOST,
+	port: REDIS_PORT,
+	password: REDIS_PASSWORD,
+	tls: {
+		servername: REDIS_HOST
+	}
+}
+
+// See redis client readme for details:
+// https://github.com/NodeRedis/node-redis#rediscreateclient
+const retryStrategy = function (opts) {
+	if (opts.error) {
+		console.error(opts.error)
+		if (opts.error.code === 'ECONNREFUSED') {
+			return new Error('The server refused the connection')
+		}
+	}
+
+	if (opts.total_retry_time > 1000 * 60 * 60) {
+		return new Error('Retry time exhausted')
+	}
+
+	if (opts.attempt > 10) {
+		return undefined
+	}
+
+	return Math.min(opts.attempt * 100, 3000)
+}
+
 const options = {
 	secret: SESSION_SECRET,
 	resave: true,
 	saveUninitialized: false,
-	retry_strategy: function (options) {
-		if (options.error && options.error.code === 'ECONNREFUSED') {
-				// End reconnecting on a specific error and flush all commands with
-				// a individual error
-				return new Error('The server refused the connection')
-		}
-		if (options.total_retry_time > 1000 * 60 * 60) {
-				// End reconnecting after a specific timeout and flush all commands
-				// with a individual error
-				return new Error('Retry time exhausted')
-		}
-		if (options.attempt > 10) {
-				// End reconnecting with built in error
-				return undefined
-		}
-		// reconnect after
-		return Math.min(options.attempt * 100, 3000)
-	}
-}
-
-const serverOpts = {
-	host: REDIS_HOST,
-	port: REDIS_PORT
-}
-
-if (REDIS_PASSWORD) {
-	Object.assign(serverOpts, { password: REDIS_PASSWORD })
+	retry_strategy: retryStrategy
 }
 
 if (REDIS_HOST !== '') {
-	const client = redis.createClient(serverOpts)
-	options.store = new RedisStore({ client: client })
+	options.store = new RedisStore({
+		client: redis.createClient(clientOpts)
+	})
 }
 
 module.exports = session(options)
