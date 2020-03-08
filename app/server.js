1998e800d2edd54f658f108d8e86e64b87ff9414
Julie Ng <me@julie.io>
2020-03-08 09:19:02 +0100

    feat(monitoring): add azure application insights

--- app/server.js
+++ app/server.js
@@ -8,11 +8,23 @@ if (process.env.NODE_ENV === 'development') {
 	require('dotenv').config()
 }
 
+const APP_INSIGHTS_INSTRUMENTATION_KEY = (process.env.APP_INSIGHTS_INSTRUMENTATION_KEY)
+	? process.env.APP_INSIGHTS_INSTRUMENTATION_KEY
+	: false
+
+if (APP_INSIGHTS_INSTRUMENTATION_KEY) {
+	const appInsights = require('applicationinsights')
+	appInsights
+		.setup(process.env.APP_INSIGHTS_INSTRUMENTATION_KEY)
+		.start()
+}
+
 const port = process.env.PORT || 3000
 const http = require('http')
 const app = require('./app')
 const server = http.createServer(app)
 
 server.listen(port, () => {
+	console.log('')
 	console.log(`Listening on port ${port}`)
 })
